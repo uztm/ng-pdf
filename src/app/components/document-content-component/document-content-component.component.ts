fda2cac394f616fc7adba4ca89ad38d96c165546
Zufarbek <game123qwerty00@gmail.com>
2025-04-22 10:06:23 +0500

    add new table

--- src/app/components/document-content-component/document-content-component.component.ts
+++ src/app/components/document-content-component/document-content-component.component.ts
@@ -1,38 +1,19 @@
-import { Component } from '@angular/core';
-import {NgForOf} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Data } from './data';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-document-content-component',
-  template: `
-    <div class="document-container">
-      <h1>My Dynamic Document</h1>
-
-      <table>
-        <thead>
-        <tr>
-          <th>#</th>
-          <th>Topic</th>
-          <th>Description</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr *ngFor="let item of data; let i = index">
-          <td>{{ i + 1 }}</td>
-          <td>{{ item.topic }}</td>
-          <td>{{ item.description }}</td>
-        </tr>
-        </tbody>
-      </table>
-    </div>
-  `,
-  imports: [
-    NgForOf
-  ],
+  templateUrl: "./document-content-component.component.html",
+  imports: [NgFor],
   styleUrls: ['./document-content-component.component.css']
 })
-export class DocumentContentComponent {
-  data = Array.from({ length: 100 }, (_, i) => ({
-    topic: `Topic ${i + 1}`,
-    description: `Description for topic ${i + 1}`
-  }));
+export class DocumentContentComponent implements OnInit {
+
+  Data = Data
+  ngOnInit(){
+    console.log(this.Data)
+
+  }
 }
