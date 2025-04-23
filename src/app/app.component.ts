5a7fbe79f7411a38b9e61547ad758b9861370b82
tmbekzod <tmcoderuz@gmail.com>
2025-04-23 10:09:09 +0500

    Lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
-
+import {NgDocumentReaderComponent} from 'ng-document-reader';
 import {DocumentContentComponent} from './components/document-content-component/document-content-component.component';
-import {NgReportContainerComponent} from './components/ng-report-container/ng-report-container.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ DocumentContentComponent, NgReportContainerComponent],
+  imports: [NgDocumentReaderComponent, DocumentContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Document no-0012190506';
-
-
-  orientation: 'portrait' | 'landscape' = 'portrait';
-
+  title = 'ng-pdf';
 }
